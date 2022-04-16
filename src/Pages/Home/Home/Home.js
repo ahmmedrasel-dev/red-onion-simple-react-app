0080e505db6c1c07006fa49c8137522e07938686
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-04-16 14:06:49 +0300

    tailwand reinstall

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>This is Home</h1>
+      <h1 className="text-3xl font-bold text-center">
+        Hello world!
+      </h1>
     </div>
   );
 };
