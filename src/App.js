9b3d8ace3032d511ed47c95a720639777136f0d9
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-04-16 14:36:13 +0300

    Header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
+import Header from './Pages/Common/Header/Header';
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
--- src/Pages/Common/Header/Header.js
+++ src/Pages/Common/Header/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
+import logo from '../../../Images/logo2.png';
 
 const Header = () => {
   return (
-    <div>
-
-    </div>
+    <nav className='md:container md:mx-auto'>
+      <div className='flex justify-between items-center py-3'>
+        <div className="nav-brand">
+          <img className='w-40' src={logo} alt="" />
+        </div>
+        <div className='flex items-center'>
+          <Link className='m-3' to='/cart'><FiShoppingCart className='text-2xl' /></Link>
+          <Link className='m-3' to='/login'>Login</Link>
+          <Link className='m-3 px-4 text-white py-2 bg-rose-500 rounded-full' to='register'>Register</Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
